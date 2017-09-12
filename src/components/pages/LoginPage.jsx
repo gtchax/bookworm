631b738e4be5a6ea86e9e4249150a0bd8a24f5fc
Gwinyai Chakonda <gtchax@gmail.com>
2017-09-12 19:41:33 +0800

    Redux store setup

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import LoginForm from '../forms/LoginForm.jsx';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { login } from '../actions/auth';
 
 
 class LoginPage extends Component {
-  submit() {
-    return this.data;
-  }
+  submit = data =>
+    this.props.login(data).then(() => this.props.history.push("/"));
+  
   render() {
     return (
       <div >
@@ -18,4 +21,11 @@ class LoginPage extends Component {
   }
 }
 
-export default LoginPage;
+LoginPage.propTypes = {
+  history: propTypes.shape({
+    push: propTypes.func.isRequired
+  }).isRequired,
+  login: propTypes.func.isRequired
+}
+
+export default connect(null, { login })(LoginPage)
